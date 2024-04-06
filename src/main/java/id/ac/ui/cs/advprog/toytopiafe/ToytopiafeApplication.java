426cb62fadf098a10b9a21e07e786fd94284b834
Bimantoro <bimantoro.widyadana@ui.ac.id>
2024-04-06 20:10:25 +0700

    initialize

---
+++ src/main/java/id/ac/ui/cs/advprog/toytopiafe/ToytopiafeApplication.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.toytopiafe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToytopiafeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToytopiafeApplication.class, args);
+    }
+
+}
